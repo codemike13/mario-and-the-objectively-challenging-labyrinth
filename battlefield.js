4267ab406ed1de78787a427e90b18c9b328ad2bd
Anthony Michael Stephens <mike@ThE-MacBook-Pro-7.local>
2019-08-20 05:24:42 -0400

    Added more pics for attacks and stats progress bars

---
+++ battlefield.js
@@ -0,0 +1,4 @@
+const marineBolter = document.querySelector(".weapon1");
+const marineHeavy = document.querySelector(".weapon1");
+const marine = document.querySelector(".weapon1");
+const marineBolter = document.querySelector(".weapon1");
